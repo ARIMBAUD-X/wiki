b67e7ff34e44d981ab8851fe4bf5bd8bb84d1bce
ARIMBAUD <arimbaud.main@gmail.com>
2023-07-07 17:22:27 +0100

    added: oop pages; oop reliant: search, page view; markdown conversion to html; display of html; consistent behaviour. removed more cached files

--- encyclopedia/models.py
+++ encyclopedia/models.py
@@ -1,13 +1,27 @@
 from django.db import models
+import markdown2
 from . import util
 # Create your models here.
 
 class wikiPage(models.Model):
     def __init__(self, title):
-        for pages in util.list_entries():
-            if pages.lower == title.lower:
-                self.title = pages
-                break
-            else:
-                self.title = False
-        self.body = util.get_entry(self.title)
+        entries = util.list_entries()
+        if title.capitalize() in entries or title.upper() in entries or title.lower() in entries: # It just works -- T.H. -- for loop = fubar
+            self.title = title
+        else:
+            self.title = False
+
+    def markdownToHtml(self):
+        markdown = util.get_entry(self.title)
+        match markdown:
+            case None | "" | False:
+                return f"There is no entry for {self.title}"
+            case other:
+                return markdown2.markdown(markdown)
+
+"""
+    def convertMarkdown(self):
+        markdownData = util.get_entry(self.title)
+        convertedData = markdown.convert(markdownData)
+        self.body = convertedData
+"""
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render, HttpResponse
-
-from . import util, models
+from django.shortcuts import render
+from .models import wikiPage
+from . import util
 
 def index(request):                                             
     return render(request, "encyclopedia/index.html", {         # goto index
@@ -8,8 +8,18 @@ def index(request):
     })
 
 def page(request, title):                                       # attempt to display a new page
-    newpage = models.wikiPage(title)
-    if newpage.title != False:
+    newpage = wikiPage(title)  
+    if newpage.title == False:
+        return render(request, "encyclopedia/notfound.html", {
+            "title": title
+        })           
+    else:
+        return render(request, "encyclopedia/page.html", {
+            "pageData": newpage.markdownToHtml,
+            "title": newpage.title
+        })               # 
+""" 
+if newpage.title != False:
         return render(request, "encyclopedia/page.html", {
            "pageData": newpage.body,
            "title": newpage.title
@@ -18,17 +28,30 @@ def page(request, title):                                       # attempt to dis
         return render(request, "encyclopedia/notfound.html",{
             "title": title
         })
+"""
 
 def search(request):
     query = request.GET.get("q")
-    if query == None or "":
-        index(request)
-    else:
-        searching = models.wikiPage(query)
-        if searching.title == False:
-            return render(request, "encyclopedia/search.html", {
+    # converts when it should not
+    """if blank go to index
+    if not blank but not an entry, go to search
+    else if not blank and an entry, go to entry"""
+    if query == None or query == "" or query == False:
+        return index(request)
+    elif util.get_entry(query) == None:  # must be false, not none
+        """searchingPage = wikiPage(query)
+        match searchingPage.title:
+            case False, None, "": ## catch error?
+                return index(request)
+            case other:"""
+        return render (request, "encyclopedia/search.html",{
             "title": query,
             "entries": util.list_entries()
         })
-        else:
-            page(query)
+    else:
+        return page(request, query) # doesnt quite redirect?
+        """if searchingPage.title == False or :
+            return render(request, "encyclopedia/search.html", {
+            "title": query,
+            "entries": util.list_entries()
+        })"""
